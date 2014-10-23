95f80aa110c5df4cd92169ff5cc36051ac0d72e1
msacred67 <msacred67@gmail.com>
2014-10-23 16:43:58 +0700

    Update client.c

--- client.c
+++ client.c
@@ -3,9 +3,10 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
+#include<sys/sendfile.h>
+#include<sys/stat.h>
 
 int main(int argc, char *argv[])
 {
@@ -19,7 +20,7 @@ int main(int argc, char *argv[])
     short dataport;
     char *serverip;
     char *command[100];
-    char *p;
+    char *p, *f;
     char buf[100];
 
     serverip = "127.0.0.1";
@@ -33,12 +34,6 @@ int main(int argc, char *argv[])
     }
     else printf("Berhasil Membuat Socket\n");
 
-    if((datasock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        perror(strerror(errno));
-        exit(-1);
-    }
-    else printf("Berhasil Membuat Data Socket\n");
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
@@ -52,16 +47,63 @@ int main(int argc, char *argv[])
     }
     else printf("Koneksi Berhasil\n");
 
+    dataport = 20;
+    bzero(&dataaddr, sizeof(dataaddr));
+    dataaddr.sin_family = AF_INET;
+    dataaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    dataaddr.sin_port = htons(servport);
 
-    int filehandle, size, status;
+    bind(datasock,(struct sockaddr *) &dataaddr, sizeof(dataaddr));
+    listen(datasock, 3);
+
+    p = strcpy(command, "USER msacred67\r\n");
+    send(sock, command, strlen(command), 0);
+    p= strcpy(command, "PASS dilarangmasuk\r\n");
+    send(sock, command, strlen(command), 0);
+
+    int filehandle, size, status,abc;
     char filename[20];
-    printf("Enter filename to put to server: ");
+    while(1)
+    {
+    printf("1- download\n2- upload\n3- quit\n");
+      scanf("%d", &abc);
+      switch(abc)
+	{
+	case 1:
+	  printf("Masukkan nama file: ");
+	  scanf("%s", filename);
+	  strcpy(buf, "get ");
+	  strcat(buf, filename);
+	  send(sock, buf, 100, 0);
+	  recv(sock, &size, sizeof(int), 0);
+	  if(!size)
+	    {
+	      printf("File tidak ditemukan\n\n");
+	    break;
+	    }
+	  f = malloc(size);
+	  recv(sock, f, size, 0);
+	  while(1)
+	    {
+	      filehandle = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0666);
+	      if(filehandle == -1)
+		{
+		  sprintf(filename + strlen(filename), "%d", 1);
+		}
+	      else break;
+	    }
+	  write(filehandle, f, size, 0);
+	  close(filehandle);
+	  system(buf);
+	  break;
+	case 2:
+	  printf("Masukkan nama file: ");
           scanf("%s", filename);
 	  filehandle = open(filename, O_RDONLY);
           if(filehandle == -1)
             {
-              printf("No such file on the local directory\n\n");
-              return 0;
+              printf("File tidak ditemukan\n\n");
+              break;
             }
           strcpy(buf, "put ");
 	  strcat(buf, filename);
@@ -72,7 +114,22 @@ int main(int argc, char *argv[])
 	  sendfile(sock, filehandle, NULL, size);
 	  recv(sock, &status, sizeof(int), 0);
 	  if(status)
-	    printf("File stored successfully\n");
+	    printf("File berhasil disimpan\n");
 	  else
-	    printf("File failed to be stored to remote machine\n");
+	    printf("gagal\n");
+	  break;
+    case 3:
+	  strcpy(buf, "quit");
+          send(sock, buf, 100, 0);
+          recv(sock, &status, 100, 0);
+	  if(status)
+	    {
+	      printf("Koneksi ditutup\n");
+	      close(sock);
+	      close(datasock);
+	      exit(0);
+	    }
+	    printf("gagal menutup koneksi\n");
+	  }
+    }
 }
